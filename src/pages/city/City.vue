709cf8edc0fde22fed6cfac5f86843256128302c
dinglei <ding5971158@163.com>
2018-09-20 18:38:27 +0800

    增加城市跳转

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <Header></Header>
+    <Search></Search>
+  </div>
+</template>
+<script>
+import Header from './components/Header'
+import Search from './components/Search'
+
+export default {
+  name: 'City',
+  components: {
+    Header,
+    Search
+  }
+}
+</script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="butotn-list">
+        <div class="button-wrapper">
+          <div class="button">深圳</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="butotn-list">
+        <div class="button-wrapper">
+          <div class="button">深圳</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="butotn-list">
+        <div class="button-wrapper">
+          <div class="button">深圳</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="stylus" scoped>
+</style>
