05dc1ecf1519dd45f612082cb9a6df528906c6f2
zikunDream <ding5971158@163.com>
2018-09-30 00:08:16 +0800

    兄弟组件联动

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,14 +1,15 @@
 <template>
   <ul class="list">
     <li class="item"
-        v-for='(item , key) of cities'
-        :key="key"
+        v-for='item  of letters'
+        :key="item"
+        :ref="item"
         @click="handleLetterClick"
         @touchstart.prevent="handleTouchStart"
         @touchmove="handleTouchMove"
         @touchend="handleTouchEnd"
     >
-      {{key}}
+      {{item}}
     </li>
   </ul>
 </template>
@@ -32,11 +33,27 @@ export default {
     handleTouchStart () {
       this.touchStatus = true
     },
-    handleTouchMove () {
-
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
     },
     handleTouchEnd () {
-
+      this.touchStatus = false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
     }
   }
 }
